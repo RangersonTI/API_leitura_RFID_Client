0e8306b0a2c6e023c32b1ed7b1189069da008bfb
Rangerson Clemente <rangerson14@outlook.com>
2024-11-16 06:18:37 -0300

    adicionado script para auto execução da aplicação

---
+++ service_readRfid.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "C:\Users\Rangerson TI\Documents\GitHub\Aplicacao_de_leitura_RFID_Client"
+call venv\Scripts\activate
+
+echo Ambiente virtual iniciado.
+
+
+#start pip install django-extensions
+#start pip install pyOpenSSL
+#start pip install Werkzeug
+
+#start py manage.py runserver_plus localhost:8000 --cert-file=ssl.crt --key-file=ssl.key
+
+start py manage.py runserver localhost:8000
+echo Servidor local iniciado.
+exit
