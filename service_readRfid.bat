6e8a97db798a30925bafb366bfc7361f860b41c2
Rangerson Clemente <rangerson14@outlook.com>
2024-11-21 06:48:20 -0300

    Update service_readRfid.bat

--- service_readRfid.bat
+++ service_readRfid.bat
@@ -4,13 +4,12 @@ call venv\Scripts\activate
 
 echo "Ambiente virtual iniciado."
 
+pip install pyserial
+pip install django
+pip install django-cors-headers
 
-#start pip install django-extensions
-#start pip install pyOpenSSL
-#start pip install Werkzeug
+echo "Dependencias instaladas com sucesso."
 
-#start py manage.py runserver_plus localhost:8000 --cert-file=ssl.crt --key-file=ssl.key
-
-start py manage.py runserver localhost:8000
+start py manage.py runserver localhost:1524
 echo "Servidor local iniciado."
 exit
